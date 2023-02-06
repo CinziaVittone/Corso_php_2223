87e1063fe71c429b299291cb4ada5fd2ee766877
Cinzia Vittone <vittone.cinzia.cv@gmail.com>
2023-02-06 10:33:32 +0100

    06/02/23

---
+++ esercitazione_php_01/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercitazione 01</title>
+</head>
+<body>
+    <?php
+
+    $array2ul=array("Rossso", "Verde", "Viola");
+
+    function array2ul($array){
+        
+        $list="<ul>";
+        for($i=0; $i < count($array); $i++){
+            $list .= "<li>$array[$i]</li>";
+        }
+        $list .= "</ul>";
+
+        return $list;
+    }
+
+    //restituisce una list che contiene una stringa
+    echo array2ul($array2ul);
+
+    ?>
+    
+</body>
+</html>
---
+++ form_in_php/login_response.php
@@ -0,0 +1,14 @@
+<h1>Sono la risposta (RESPONSE)</h1>
+
+<?php
+
+//$_GETè un array superglobale che esiste nel sistema
+//tag che impagina l' array
+echo "<pre>";
+echo "get: ";
+print_r($_GET);//array super globale del get
+echo "post: ";
+print_r($_POST);//array super globale del post
+echo "</pre>";
+
+?>
