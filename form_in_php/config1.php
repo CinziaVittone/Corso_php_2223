877ce4ef41f491cf8400611b36c31053bdfdd6a6
Cinzia Vittone <vittone.cinzia.cv@gmail.com>
2023-04-15 10:14:29 +0200

    config1_script_localhost

--- form_in_php/config1.php
+++ form_in_php/config1.php
@@ -1,21 +1,27 @@
 <?php
-//stringa che sarà il nome della costante
-//che useremo per connetterci
-//definiamo delle costanti
-/*credenziali per lavoro in locale*/
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "form_in_php");
+//print_r($_SERVER); //mostra le caratteristiche del mio server
 
+$host = $_SERVER['HTTP_HOST'];
 
+if($host == 'localhost'){
+    //stringa che sarà il nome della costante
+    //che useremo per connetterci
+    //definiamo delle costanti
+    /*credenziali per lavoro in locale*/
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASSWORD", "");
+    define("DB_NAME", "form_in_php");
+}
 
-/*credenziali date dall'host online
-define("DB_HOST", "localhost");
-define("DB_USER", "id20599766_cinzia_vittone");
-define("DB_PASSWORD", "Ciccia.5555!");
-define("DB_NAME", "id20599766_form_in_php");
-*/
+if($host == 'cinziavittone.000webhostapp.com'){
+    /*credenziali date dall'host online*/
+    define("DB_HOST", "localhost");
+    define("DB_USER", "id20599766_cinzia_vittone");
+    define("DB_PASSWORD", "Ciccia.5555!");
+    define("DB_NAME", "id20599766_form_in_php");
+
+}
 
 //valido per entrambi
 define("DB_DSN", "mysql:host=".DB_HOST.";dbname=".DB_NAME);
