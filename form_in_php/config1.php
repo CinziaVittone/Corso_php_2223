6a82bfcdf2248bc6992e6b7714394b61faa64490
Cinzia Vittone <vittone.cinzia.cv@gmail.com>
2023-04-15 09:13:05 +0200

    config1_in_locale

--- form_in_php/config1.php
+++ form_in_php/config1.php
@@ -7,7 +7,7 @@ define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
 define("DB_NAME", "form_in_php");
-define("DB_DSN", "mysql:host=".DB_HOST.";dbname=".DB_NAME);
+
 
 
 /*credenziali date dall'host online
@@ -16,5 +16,9 @@ define("DB_USER", "id20599766_cinzia_vittone");
 define("DB_PASSWORD", "Ciccia.5555!");
 define("DB_NAME", "id20599766_form_in_php");
 */
+
+//valido per entrambi
+define("DB_DSN", "mysql:host=".DB_HOST.";dbname=".DB_NAME);
+
 
 ?>
